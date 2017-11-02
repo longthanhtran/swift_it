bb4f5cc46c129aa9ff1af539b2142a8908cdf33b
Long Tran <long.thanh.tran@gmail.com>
2017-11-02 13:41:32 +0700

    Move closures to its own file

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -25,25 +25,6 @@ if CommandLine.arguments.count != 2 {
     })
     print(result)
 
-    let zero_for_odd = numbers.map({ (number: Int) -> Int in
-        var result: Int = number
-        if number % 2 != 0 {
-            result = 0
-        }
-        return result
-    })
-    print(zero_for_odd)
-
-    let mappedNumbers = numbers.map({ number in 3 * number })
-    print(mappedNumbers)
-
-    let sortedNumbers = numbers.sorted { $0 > $1 }
-    print(sortedNumbers)
-
-    let shape = Shape()
-    shape.numberOfSides = 7
-    let shapeDescription = shape.simpleDescription()
-    print(shapeDescription)
 } else {
     let name = CommandLine.arguments[1]
     sayHello(name: name)
