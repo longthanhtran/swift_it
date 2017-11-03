8a8ae140a62e228e085b91e3002b49476f376096
Long Tran <long.thanh.tran@gmail.com>
2017-11-03 10:12:07 +0700

    group loops together

--- Sources/Hello/Loops.swift
+++ Sources/Hello/Loops.swift
@@ -18,3 +18,26 @@ func forInGame() {
 
     print("The largest number \(largest) is of type \(largestType as String?)")
 }
+
+func whileRun() {
+    var n = 2
+    while n < 100 {
+        n *= 2
+    }
+
+    print(n)
+
+    var m = 2
+    repeat {
+        m *= 2
+    } while m < 100
+
+    print(m)
+
+    var total = 0
+    for i in 0...4 { // may use ..< for exclusive range of upper value
+        total += i
+    }
+
+    print(total)
+}
